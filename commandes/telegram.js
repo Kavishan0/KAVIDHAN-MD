6c851f42768ad340063d9f5317aec6bf940dd0ab
KaVISHAN <164993030+Kavishan0@users.noreply.github.com>
2024-06-26 10:09:01 +0530

    💀❗KAVISHAN-MD💀❗

--- commandes/telegram.js
+++ commandes/telegram.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "telg", reaction: "✍️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*KAVISHAN-MD* Telegram channel \n\n ' + "Click the links below to join telegram channel of Lazack-md support \n\n";
+    let z = '*KAVISHAN-MD* Telegram channel \n\n ' + "Click the links below to join telegram channel of kavishan-md support \n\n";
     let d = ' 📡 https://t.me/hackersreality';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/4415fe12b1203e4caeecf.jpg';
