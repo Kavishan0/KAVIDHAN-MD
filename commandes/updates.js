142c6b491ca05e69d6516d4191a016e2f26a9027
KaVISHAN <164993030+Kavishan0@users.noreply.github.com>
2024-06-30 09:25:15 +0530

    💀❗KAVISHAN-MD💀❗

---
+++ commandes/updates.js
@@ -0,0 +1,127 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 👋\n\nClick The link below to Join 𝗞𝗔𝗩𝗜𝗦𝗛𝗔𝗡 𝗠𝗗 WhatsApp Testing Group\n\n';
+    let d = 'https://chat.whatsapp.com/KYAN2uzltDYFLxK0qLGcm5';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 👋\n\nClick The link below to Join 𝗞𝗔𝗩𝗜𝗦𝗛𝗔𝗡 𝗠𝗗 WhatsApp Channel\n\n';
+    let d = 'https://chat.whatsapp.com/KYAN2uzltDYFLxK0qLGcm5';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "developer", reaction: "🌟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : Brian Kanyiri Wainaina\n✇ *AGE* : 17 years\n✇ : MALE\n✇ *RELIGION* : sri lanka\n✇ *NATIONALITY* :sinhala🦁\n✇ *FAV TEAM* :Manchester United\n✇ *REAL LIFE* :I am a plumber, I never learned about coding.\n✇ *HOBBY* : Scrolling on websites\n✇ *CONTACT KAVISHAN* :👇 👇 \n';
+    let d = '𓄂 https://wa.me/94789474773\n𓄂 https://wa.me/94788017991\n\n *THANK YOU FOR LOVING KAVISHAN-MD*. ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/dd11d4211249e5eac9dfd.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "test", reaction: "✅", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *KAVISHAN-MD BOT IS CURRENTLY ACTIVE*\n';
+    let d = ' ❄︎ *_MADE ON EARTH BY KAVISHAN* ❄︎';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/dd11d4211249e5eac9dfd.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bot", reaction: "❗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *KAVISHAN-MD MAIN FEATURES* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n\n';
+    let d = ' *THANK YOU FOR LOVING KAVISHAN-MD*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "g*y", reaction: "🦁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *F*CK MEHN,,, THIS GUY IS G*Y🦁*\n';
+    let d = 'WHY WOULD YOU REQUEST THAT COMMAND IF NOT';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/5d4c27c77b240483e7b05.png';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "check", reaction: "🗿", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *KAVISHAN-MD UPDATES*\n\n';
+    let d = 'Checking for updates...⏳\n\nUpdating new version...⏳\n\ndone....✅\n\n *KAVISHAN MD NEW VERSION SUCCESSFULLY INSTALLED...* ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "version", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *YOU ARE CURRENTLY USING KAVISHAN-MD*\n\n';
+    let d = '✅latest version installed...';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bugmenu", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '┌─❒𝗞𝗔𝗩𝗜𝗦𝗛𝗔𝗡 𝗠𝗗 𝗕𝗨𝗚 𝗠𝗘𝗡𝗨⁠⁠⁠⁠❒⁠⁠⁠\n│╭─────────────···\n┴│▸\n⬡│▸ ʙᴜɢ\n⬡│▸ ʙᴜɢᴠ\n⬡│▸ ʙᴜɢʀᴇᴀᴄᴛ\n⬡│▸ ᴀᴜᴅɪᴏʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢᴠ\n┬│▸\n│╰────────────···▸▸\n';
+    let d = '└───────────────···▸';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "genz", reaction: "🦁", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *❒⁠⁠⁠⁠ #REJECTFINANCEBILL2024 ❒⁠⁠⁠⁠*\n\nClick The link below to Join *❒⁠⁠⁠⁠GEN Z❒* official⁠⁠⁠⁠ WhatsApp Channel\nHelp us grow and reject that piece of shiet.\n\n';
+    let d = 'https://chat.whatsapp.com/KYAN2uzltDYFLxK0qLGcm5';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/4b02a28e156b80d978589.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
+      let varmess=z+d
+      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
